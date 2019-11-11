32f199bff57a400d5f4321a2864ec3bbefb8be97
kimHJ <khj64076407@gmail.com>
2019-11-11 22:58:36 +0900

    commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hi!");
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening on: http://localhost:4000${PORT}`);
+}
+
+function handleHome(req, res) {
+  res.send("Hello from home");
+}
+
+function handleProfile(req, res) {
+  res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(4000, handleListening);
