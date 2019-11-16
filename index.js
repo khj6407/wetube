931f51410ab0d7d6f467e55b2564431568213be3
kimHJ <khj64076407@gmail.com>
2019-11-16 19:01:13 +0900

    middleawares

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const PORT = 4000;
 const handleListening = () =>
   console.log(`Listening on: http://localhost:4000${PORT}`);
 
-const handleHome = (req, res) => res.send("Hello from ass");
+const handleHome = (req, res) => res.send("Hello from my ass");
 
 const handleProfile = (req, res) => res.send("You are on my profile"); //arrow function
+
+const betweenHome = (req, res, next) => {
+  console.log("Between");
+  next();
+};
+
+app.use(betweenHome);
 
 app.get("/", handleHome);
 
